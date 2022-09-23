82e9c808faf7d2f8d36ac09930e8aa396b9cf849
Constantine <kosta.tovstik@gmail.com>
2022-09-23 17:23:08 +0300

    removed logger, added /self mapping for whoami purposes (requires a jwt), returns a UserInfo class. default get mapping now requires a path variable for querying other users by username

--- src/main/java/sirkostya009/posterapp/api/UserApi.java
+++ src/main/java/sirkostya009/posterapp/api/UserApi.java
@@ -1,18 +1,16 @@
 package sirkostya009.posterapp.api;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import sirkostya009.posterapp.model.AppUser;
 import sirkostya009.posterapp.model.Credentials;
+import sirkostya009.posterapp.model.UserInfo;
 import sirkostya009.posterapp.service.UserService;
 import sirkostya009.posterapp.util.JwtUtils;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
@@ -32,14 +30,18 @@ public class UserApi {
             throw new RuntimeException("Login or password incorrect", e);
         }
 
-        var user = service.loadUserByUsername(credentials.getLogin());
+        var user = service.findByLogin(credentials.getLogin());
 
         return jwtUtils.generateToken(user);
     }
 
-    @GetMapping
-    public ResponseEntity<AppUser> getUser(@RequestParam(name = "username") String username) {
-        log.info(service.findByUsername(username).toString());
-        return ResponseEntity.ok(service.findByUsername(username));
+    @GetMapping("/{username}")
+    public UserInfo getUser(@PathVariable String username) {
+        return UserInfo.fromAppUser((AppUser) service.loadUserByUsername(username));
+    }
+
+    @GetMapping("/self")
+    public UserInfo getUserInfo(UsernamePasswordAuthenticationToken token) {
+        return UserInfo.fromAppUser((AppUser) token.getPrincipal());
     }
 }
