b06b91ebf86f89237e27e648e8c2054d579f3a6b
Constantine <kosta.tovstik@gmail.com>
2022-09-23 17:17:32 +0300

    refactored save and findAll methods for updated rest api, also added a postsPerSlice variable

--- src/main/java/sirkostya009/posterapp/service/PosterService.java
+++ src/main/java/sirkostya009/posterapp/service/PosterService.java
@@ -1,28 +1,34 @@
 package sirkostya009.posterapp.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import sirkostya009.posterapp.model.AppUser;
 import sirkostya009.posterapp.model.Poster;
+import sirkostya009.posterapp.model.PosterModel;
 import sirkostya009.posterapp.repo.PosterRepo;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PosterService {
 
     private final PosterRepo repo;
+    private final static int postsPerSlice = 10;
 
     public Poster getPoster(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new RuntimeException("poster with " + id + " not found"));
     }
 
-    public Poster save(Poster poster) {
-        return repo.save(poster);
+    public Poster save(PosterModel poster, AppUser user) {
+        return repo.save(new Poster(
+                poster.getContent(),
+                user.getUsername()
+        ));
     }
 
-    public List<Poster> findAll() {
-        return repo.findAll();
+    public Page<Poster> findAll(int page) {
+        return repo.findAll(PageRequest.of(page, postsPerSlice));
     }
 }
