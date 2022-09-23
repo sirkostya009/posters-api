b7f19612b6afff57c7f12f575ecd133187a8d4d1
Constantine <kosta.tovstik@gmail.com>
2022-09-23 17:19:32 +0300

    UserInfo class

---
+++ src/main/java/sirkostya009/posterapp/model/UserInfo.java
@@ -0,0 +1,21 @@
+package sirkostya009.posterapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public @Data class UserInfo {
+    private long id;
+    private String email;
+    private String username;
+
+    public static UserInfo fromAppUser(AppUser user) {
+        return new UserInfo(
+                user.getId(),
+                user.getEmail(),
+                user.getUsername()
+        );
+    }
+}
