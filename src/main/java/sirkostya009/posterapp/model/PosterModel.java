d5d96d77c2534786ab10df4cdd7023950cd0da0b
Constantine <kosta.tovstik@gmail.com>
2022-09-25 20:48:47 +0300

    added new stuff to PosterModel

--- src/main/java/sirkostya009/posterapp/model/PosterModel.java
+++ src/main/java/sirkostya009/posterapp/model/PosterModel.java
@@ -1,7 +1,15 @@
 package sirkostya009.posterapp.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public @Data class PosterModel {
-    private String content;
+    private String text;
+    private UserInfo author;
+    private boolean isLikedByYou;
+    private int likes;
+    private Long id;
 }
