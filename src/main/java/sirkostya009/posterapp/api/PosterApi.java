07de8efaa3205004eb7c9c46f727877abf9c4e42
Constantine <kosta.tovstik@gmail.com>
2022-09-25 20:48:32 +0300

    posters api now returns PosterModel instead of raw Poster class to provide clients with more info

--- src/main/java/sirkostya009/posterapp/api/PosterApi.java
+++ src/main/java/sirkostya009/posterapp/api/PosterApi.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import sirkostya009.posterapp.model.AppUser;
-import sirkostya009.posterapp.model.Poster;
 import sirkostya009.posterapp.model.PosterModel;
 import sirkostya009.posterapp.service.PosterService;
 
@@ -17,19 +16,28 @@ public class PosterApi {
     private final PosterService service;
 
     @PostMapping
-    public Poster post(@RequestBody PosterModel poster, UsernamePasswordAuthenticationToken token) {
-        return service.save(poster, (AppUser) token.getPrincipal());
+    public PosterModel post(@RequestBody String posterText, UsernamePasswordAuthenticationToken token) {
+        return service.save(posterText, userFromToken(token));
     }
 
     @GetMapping
     @RequestMapping("/{id}")
-    public Poster getById(@PathVariable Long id) {
+    public PosterModel getById(@PathVariable Long id) {
         return service.getPoster(id);
     }
 
     @GetMapping
-    public Page<Poster> all(@RequestParam(value = "page", defaultValue = "0") int page) {
+    public Page<PosterModel> all(@RequestParam(value = "page", defaultValue = "0") int page) {
         return service.findAll(page);
+    }
+
+    @GetMapping("/like/{id}")
+    public boolean like(@PathVariable long id, UsernamePasswordAuthenticationToken token) {
+        return service.likePoster(id, userFromToken(token));
+    }
+
+    private AppUser userFromToken(UsernamePasswordAuthenticationToken token) {
+        return (AppUser) token.getPrincipal();
     }
 
 }
