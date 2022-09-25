c099919e0c484dcb66ad3460d09ed12d203093da
Constantine <kosta.tovstik@gmail.com>
2022-09-25 20:49:39 +0300

    refactored service to return PosterModel, added likePoster(Long, AppUser) func; converter from Poster to PosterModel

--- src/main/java/sirkostya009/posterapp/service/PosterService.java
+++ src/main/java/sirkostya009/posterapp/service/PosterService.java
@@ -2,11 +2,14 @@ package sirkostya009.posterapp.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sirkostya009.posterapp.model.AppUser;
 import sirkostya009.posterapp.model.Poster;
 import sirkostya009.posterapp.model.PosterModel;
+import sirkostya009.posterapp.model.UserInfo;
 import sirkostya009.posterapp.repo.PosterRepo;
 
 @Service
@@ -15,20 +18,46 @@ public class PosterService {
 
     private final PosterRepo repo;
     private final static int postsPerSlice = 10;
+    private final UserService userService;
 
-    public Poster getPoster(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new RuntimeException("poster with " + id + " not found"));
+    public PosterModel getPoster(Long id) {
+        return posterToModel(repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("poster with " + id + " not found")));
     }
 
-    public Poster save(PosterModel poster, AppUser user) {
-        return repo.save(new Poster(
-                poster.getContent(),
+    public PosterModel save(String posterText, AppUser user) {
+        return posterToModel(repo.save(new Poster(
+                posterText,
                 user.getUsername()
-        ));
+        )));
     }
 
-    public Page<Poster> findAll(int page) {
-        return repo.findAll(PageRequest.of(page, postsPerSlice));
+    public Page<PosterModel> findAll(int page) {
+        var result = repo.findAll(PageRequest.of(page, postsPerSlice));
+        var contents = result.getContent().stream().map(this::posterToModel).toList();
+        return new PageImpl<>(contents, result.getPageable(), result.getTotalElements());
+    }
+
+    @Transactional
+    public boolean likePoster(Long id, AppUser user) {
+        var poster = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("poster doesnt not exist"));
+
+        if (poster.getLikes().contains(user))
+            poster.getLikes().remove(user);
+        else poster.getLikes().add(user);
+
+        return poster.getLikes().contains(user);
+    }
+
+    private PosterModel posterToModel(Poster poster) {
+        var user = (AppUser) userService.loadUserByUsername(poster.getAuthor());
+        return new PosterModel(
+                poster.getText(),
+                UserInfo.fromAppUser(user),
+                poster.getLikes().contains(user),
+                poster.getLikes().size(),
+                poster.getId()
+        );
     }
 }
